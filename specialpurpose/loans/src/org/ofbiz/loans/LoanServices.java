6941334356df8b912eb260a14ff822259f2b6353
Japheth Odonya <jodonya@gmail.com>
2014-06-25 19:27:05 +0300

    Loan Management Changes

---
+++ specialpurpose/loans/src/org/ofbiz/loans/LoanServices.java
@@ -0,0 +1,162 @@
+package org.ofbiz.loans;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javolution.util.FastMap;
+
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.webapp.event.EventHandlerException;
+
+import com.google.gson.Gson;
+
+public class LoanServices {
+	
+	public static String getLoanDetails(HttpServletRequest request, HttpServletResponse response){
+		Map<String, Object> result = FastMap.newInstance();
+		Delegator delegator = (Delegator) request.getAttribute("delegator");
+		String saccoProductId = (String) request.getParameter("saccoProductId");
+		GenericValue saccoProduct = null;
+		
+		//SaccoProduct
+		try {
+			saccoProduct = delegator.findOne("SaccoProduct", UtilMisc.toMap("saccoProductId", saccoProductId), false);
+		} catch (GenericEntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Cannot Get Product Details";
+		}
+		
+		if (saccoProduct != null) {
+			result.put("percentInterestPerMonthAmt", saccoProduct.get("percentInterestPerMonthAmt"));
+			result.put("maxRepaymentPeriod", saccoProduct.get("maxRepaymentPeriod"));
+			result.put("loanamt", saccoProduct.get("maxAmt"));
+			//result.put("selectedRepaymentPeriod", saccoProduct.get("selectedRepaymentPeriod"));
+		} else {
+			System.out.println("######## Product details not found #### ");
+		}
+		// return JSONBuilder.class.
+		// JSONObject root = new JSONObject();
+
+		Gson gson = new Gson();
+		String json = gson.toJson(result);
+
+		// set the X-JSON content type
+		response.setContentType("application/x-json");
+		// jsonStr.length is not reliable for unicode characters
+		try {
+			response.setContentLength(json.getBytes("UTF8").length);
+		} catch (UnsupportedEncodingException e) {
+			try {
+				throw new EventHandlerException("Problems with Json encoding",
+						e);
+			} catch (EventHandlerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		// return the JSON String
+		Writer out;
+		try {
+			out = response.getWriter();
+			out.write(json);
+			out.flush();
+		} catch (IOException e) {
+			try {
+				throw new EventHandlerException(
+						"Unable to get response writer", e);
+			} catch (EventHandlerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		return json;
+	}
+
+	public static String getMember(HttpServletRequest request,
+			HttpServletResponse response) {
+		Map<String, Object> result = FastMap.newInstance();
+		// Delegator delegator = dctx.getDelegator();
+		Delegator delegator = (Delegator) request.getAttribute("delegator");
+		// request.getParameter(arg0)
+		String partyId = (String) request.getParameter("memberId");
+		// Locale locale = (Locale) request.getParameter("locale");
+		GenericValue member = null;
+		// String firstName, middleName, lastName, idNumber, memberType,
+		// memberNumber, mobileNumber;
+		String memberDetails = "";
+
+		try {
+			member = delegator.findOne("Member",
+					UtilMisc.toMap("partyId", partyId), false);
+		} catch (GenericEntityException e) {
+			// return ServiceUtil.returnError(UtilProperties.getMessage("",
+			// "Cannot Get Member Details",
+			// UtilMisc.toMap("errMessage", e.getMessage()), locale));
+			return "Cannot Get Member Details";
+		}
+		if (member != null) {
+			result.put("firstName", member.get("firstName"));
+			result.put("middleName", member.get("middleName"));
+			result.put("lastName", member.get("lastName"));
+			result.put("idNumber", member.get("idNumber"));
+			result.put("memberType", member.get("memberType"));
+			result.put("memberNumber", member.get("memberNumber"));
+			result.put("mobileNumber", member.get("mobileNumber"));
+		} else {
+			System.out.println("######## Member details not found #### ");
+		}
+		// return JSONBuilder.class.
+		// JSONObject root = new JSONObject();
+
+		Gson gson = new Gson();
+		String json = gson.toJson(result);
+
+		System.out.println("json = " + json);
+
+		// set the X-JSON content type
+		response.setContentType("application/x-json");
+		// jsonStr.length is not reliable for unicode characters
+		try {
+			response.setContentLength(json.getBytes("UTF8").length);
+		} catch (UnsupportedEncodingException e) {
+			try {
+				throw new EventHandlerException("Problems with Json encoding",
+						e);
+			} catch (EventHandlerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		// return the JSON String
+		Writer out;
+		try {
+			out = response.getWriter();
+			out.write(json);
+			out.flush();
+		} catch (IOException e) {
+			try {
+				throw new EventHandlerException(
+						"Unable to get response writer", e);
+			} catch (EventHandlerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		return json;
+
+	}
+
+}
