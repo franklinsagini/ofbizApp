ec49e5cbe2e4a0e6dd5730357729f2f33d391d2b
Japheth Odonya <jodonya@gmail.com>
2014-11-28 20:51:43 +0300

    Added Member Notification Definition and Tracking functionality

---
+++ applications/membernotification/src/org/ofbiz/membernotification/MemberNotificationServices.java
@@ -0,0 +1,27 @@
+package org.ofbiz.membernotification;
+
+public class MemberNotificationServices {
+	
+	public static String createLoanDefaulterMessage(){
+		
+		//Create One Month Defaulter Message
+		
+		//Create Two Month Defaulter Message
+		
+		//Create Three Month Defaulter Message
+		
+		return "";
+	}
+	
+	public static String sendLoanDefaulterMessage(){
+		
+		//Create One Month Defaulter Message
+		
+		//Create Two Month Defaulter Message
+		
+		//Create Three Month Defaulter Message
+		
+		return "";
+	}
+
+}
