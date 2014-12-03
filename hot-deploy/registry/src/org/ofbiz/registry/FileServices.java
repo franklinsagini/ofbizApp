9a4f22370c5f39980f749fb9a127c53df0ce0223
julian_geniuz <juliandan17@gmail.com>
2014-12-03 17:23:35 +0300

    Made changes in file movement and leave reasons in global settings

--- hot-deploy/registry/src/org/ofbiz/registry/FileServices.java
+++ hot-deploy/registry/src/org/ofbiz/registry/FileServices.java
@@ -1,12 +1,16 @@
 package org.ofbiz.registry;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.DelegatorFactoryImpl;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericModelException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
 
 public class FileServices {
 	public static String module = FileServices.class.getName();
@@ -28,6 +32,29 @@ public class FileServices {
 
 
 		return true;
+	}
+	
+	//================================ COUNTING FILE VOLUMES ====================================================
+	
+	public static String getFileVolumeCount(String partyId) {
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		String volumeCount = null;
+		int count=0;
+		List<GenericValue> volumELI = null;
+		try {
+			volumELI = delegator.findList("RegistryFileVolume", EntityCondition.makeCondition("partyId", partyId), null, null, null, false);
+		
+		} catch (GenericEntityException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		for (GenericValue genericValue : volumELI) {
+			count++;
+		}
+		volumeCount= String.valueOf(count);
+				
+		return volumeCount;
+		
 	}
 
 }
