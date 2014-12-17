6741c399e395225aa3032701bdf809be9727b973
Samoei Phil <phil@samphiltech.com>
2014-12-17 12:56:29 +0300

    Fixed Reports on File Movement and Loan Details

--- applications/humanres/webapp/humanres/WEB-INF/actions/reports/fileMovement.groovy
+++ applications/humanres/webapp/humanres/WEB-INF/actions/reports/fileMovement.groovy
@@ -1,12 +1,11 @@
 partyId = parameters.partyId
+LpartyId = partyId.toLong();
 if (partyId) {
-   employee = delegator.findOne("Member", [partyId : partyId], false);
+   employee = delegator.findOne("Member", [partyId : LpartyId], false);
    if(employee){
     context.employee = employee;
    }
-   else {
-    return
-   }
+
 context.activities = delegator.findByAnd("RegistryFileLogs", [partyId : partyId], null, false);
 context.file = delegator.findOne("RegistryFiles", [partyId : partyId], false);
 }
