af88f09bf6661e5a18f8c6255ba7866c0e379c87
Japheth Odonya <jodonya@gmail.com>
2015-01-06 01:26:26 +0300

    added loan summary report

---
+++ applications/loanreports/webapp/loanreports/WEB-INF/actions/reports/loansummaryReport.groovy
@@ -0,0 +1,34 @@
+import org.ofbiz.base.util.UtilMisc;
+
+import org.ofbiz.entity.Delegator;
+
+partyId = parameters.partyId
+stationId = parameters.stationId
+loanProductId = parameters.loanProductId
+
+if ((partyId != null) && (partyId != "")){
+	lpartyId = partyId.toLong();
+}
+
+if ((stationId != null) && (stationId != "")){
+	lstationId = stationId.toLong();
+}
+
+	if ((loanProductId != null) && (loanProductId != "")){
+	lloanProductId = loanProductId.toLong();
+}
+
+
+
+//Loans - by member or by station or by product
+disburseLoanStatusId = 6.toLong();
+if ((partyId != null) && (partyId != "")){
+	myLoansList = delegator.findByAnd("LoanApplication",  [partyId : lpartyId, loanStatusId: disburseLoanStatusId], null, false);
+}
+if ((lloanProductId != null) && (loanProductId != "")){
+	myLoansList = delegator.findByAnd("LoanApplication",  [loanProductId : lloanProductId, loanStatusId: disburseLoanStatusId], null, false);
+}
+
+context.myLoansList = myLoansList
+context.partyId = partyId
+
