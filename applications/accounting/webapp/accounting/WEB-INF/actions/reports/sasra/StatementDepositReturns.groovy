5914861c92cec72d7dbf3e2ada23a9c050171fe4
Samoei Phil <phil@samphiltech.com>
2015-02-04 10:53:35 +0300

    Completed Statement of Deposit Return

--- applications/accounting/webapp/accounting/WEB-INF/actions/reports/sasra/StatementDepositReturns.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/reports/sasra/StatementDepositReturns.groovy
@@ -1,3 +1,23 @@
-accountBalanceList = [];
-componentMap = [:]
-context.capitalAdequacyList = accountBalanceList;
+import org.ofbiz.base.util.UtilMisc;
+
+statementofDepositReturnsList = [];
+depositReturnsList = [];
+statementofDepositReturnsTotalList = [];
+totalAccounts = 0
+totalAmount = BigDecimal.ZERO
+depositReturnsList = delegator.findList("StatementDepositReturns", null, UtilMisc.toSet("rangesId", "depositTypeId", "noOfAccounts", "amount"), UtilMisc.toList("rangesId"), null, false);
+
+
+if (depositReturnsList) {
+    depositReturnsList.each { depositReturn ->
+        range = delegator.findOne("DepositReturnsRanges", UtilMisc.toMap("rangesId", depositReturn.rangesId), true);
+        depositType = delegator.findOne("DepositType", UtilMisc.toMap("depositTypeId", depositReturn.depositTypeId), true);
+        statementofDepositReturnsList.add(["minRange":range.minRange, "maxRange":range.maxRange, "depositType":depositType.name, noOfAccounts:depositReturn.noOfAccounts, amount:depositReturn.amount]);
+        totalAccounts = totalAccounts + (depositReturn.noOfAccounts).toInteger()
+        totalAmount = totalAmount + depositReturn.amount
+    }
+}
+statementofDepositReturnsTotalList.add("totalName":"Total Accounts", "balance":totalAccounts);
+statementofDepositReturnsTotalList.add("totalName":"Total Amount in KES", "balance":totalAmount);
+context.statementofDepositReturnsList = statementofDepositReturnsList;
+context.statementofDepositReturnsTotalList = statementofDepositReturnsTotalList;
