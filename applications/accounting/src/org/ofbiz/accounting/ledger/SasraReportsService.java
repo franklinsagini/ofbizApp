99fb971532a6a673cf657e485e932569982edfcb
Japheth Odonya <jodonya@gmail.com>
2015-02-12 16:05:46 +0300

    SASRA report update

--- applications/accounting/src/org/ofbiz/accounting/ledger/SasraReportsService.java
+++ applications/accounting/src/org/ofbiz/accounting/ledger/SasraReportsService.java
@@ -1,30 +1,31 @@
 package org.ofbiz.accounting.ledger;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 public class SasraReportsService {
 	
-	public static BigDecimal getReportItemTotals(String reportId, String reportItemCode){
+	public static BigDecimal getReportItemTotals(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCode){
 		
 		// TODO Fix the code
 		return BigDecimal.ZERO;
 	}
 	
 	
-	public static BigDecimal getReportItemRatio(String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
+	public static BigDecimal getReportItemRatio(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
 		
 		// TODO Fix the code
 		return BigDecimal.ZERO;
 	}
 	
 
-	public static BigDecimal getReportItemRatioPercentage(String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
+	public static BigDecimal getReportItemRatioPercentage(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
 		
 		// TODO Fix the code return as a percentage * 100
 		return BigDecimal.ZERO;
 	}
 	
-	public static BigDecimal getReportItemDifference(String reportId, String reportItemCodeMinuend, String reportItemCodeSubtrahend){
+	public static BigDecimal getReportItemDifference(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeMinuend, String reportItemCodeSubtrahend){
 		
 		// TODO Fix the code
 		
@@ -32,7 +33,7 @@ public class SasraReportsService {
 	}
 	
 
-	public static BigDecimal getReportItemSum(String reportId, String reportItemCodeFirstAddend, String reportItemCodeSecondAddend){
+	public static BigDecimal getReportItemSum(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeFirstAddend, String reportItemCodeSecondAddend){
 		
 		// TODO Fix the code
 		
