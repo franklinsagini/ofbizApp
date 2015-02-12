a907553c143d97e47db0102e8850a1e457a127bb
Japheth Odonya <jodonya@gmail.com>
2015-02-12 18:17:14 +0300

    Sasra Reports Service done

--- applications/accounting/src/org/ofbiz/accounting/ledger/SasraReportsService.java
+++ applications/accounting/src/org/ofbiz/accounting/ledger/SasraReportsService.java
@@ -1,43 +1,293 @@
 package org.ofbiz.accounting.ledger;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.ofbiz.accounting.util.UtilAccounting;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.DelegatorFactoryImpl;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionList;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
+
+/***
+ * @author Japheth Odonya
+ * 
+ *         Purpose : Get SASRA Report Values
+ * @author Japheth Odonya @when Feb 12, 2015 6:11:46 PM
+ * 
+ * */
 public class SasraReportsService {
-	
-	public static BigDecimal getReportItemTotals(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCode){
-		
-		// TODO Fix the code
-		return BigDecimal.ZERO;
-	}
-	
-	
-	public static BigDecimal getReportItemRatio(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
-		
-		// TODO Fix the code
-		return BigDecimal.ZERO;
-	}
-	
 
-	public static BigDecimal getReportItemRatioPercentage(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
-		
+	/***
+	 * Get Totals given report ID, report Item Code and the from and thru dates
+	 * */
+	public static BigDecimal getReportItemTotals(Timestamp fromDate,
+			Timestamp thruDate, String reportId, String reportItemCode) {
+
+		// TODO Fix the code
+		// Get Accounts List
+
+		// Get SasraReportItem given reportId and reportItemCode
+		String reportItemId = getReportItemId(reportId, reportItemCode);
+
+		List<String> listAccountIds = getAccountIds(reportItemId);
+
+		BigDecimal bdTotalAmount = BigDecimal.ZERO;
+
+		bdTotalAmount = getAccountTotal(fromDate, thruDate, listAccountIds);
+
+		return bdTotalAmount;
+	}
+
+	/**
+	 * Returns Ratio
+	 * */
+	public static BigDecimal getReportItemRatio(Timestamp fromDate,
+			Timestamp thruDate, String reportId,
+			String reportItemCodeNumerator, String reportItemCodeDenominator) {
+
+		// TODO Fix the code
+
+		BigDecimal bdNumeratorTotal = getReportItemTotals(fromDate, thruDate,
+				reportId, reportItemCodeNumerator);
+		BigDecimal bdDenominatorTotal = getReportItemTotals(fromDate, thruDate,
+				reportId, reportItemCodeDenominator);
+
+		if ((bdNumeratorTotal.compareTo(BigDecimal.ZERO) == 1)
+				|| (bdDenominatorTotal.compareTo(BigDecimal.ZERO) == 1))
+			return BigDecimal.ZERO;
+
+		return bdNumeratorTotal.divide(bdDenominatorTotal, 4,
+				RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Returns Percentage
+	 * */
+	public static BigDecimal getReportItemRatioPercentage(Timestamp fromDate,
+			Timestamp thruDate, String reportId,
+			String reportItemCodeNumerator, String reportItemCodeDenominator) {
+
 		// TODO Fix the code return as a percentage * 100
-		return BigDecimal.ZERO;
+		return getReportItemRatio(fromDate, thruDate, reportId,
+				reportItemCodeNumerator, reportItemCodeDenominator).multiply(
+				new BigDecimal(100));
 	}
-	
-	public static BigDecimal getReportItemDifference(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeMinuend, String reportItemCodeSubtrahend){
-		
-		// TODO Fix the code
-		
-		return BigDecimal.ZERO;
-	}
-	
 
-	public static BigDecimal getReportItemSum(Timestamp startDate, Timestamp endDate, String reportId, String reportItemCodeFirstAddend, String reportItemCodeSecondAddend){
-		
+	/**
+	 * Returns Difference between two Item Values
+	 * */
+	public static BigDecimal getReportItemDifference(Timestamp fromDate,
+			Timestamp thruDate, String reportId, String reportItemCodeMinuend,
+			String reportItemCodeSubtrahend) {
+
 		// TODO Fix the code
-		
-		return BigDecimal.ZERO;
+		BigDecimal bdDifference = getReportItemTotals(fromDate, thruDate,
+				reportId, reportItemCodeMinuend).subtract(
+				getReportItemTotals(fromDate, thruDate, reportId,
+						reportItemCodeSubtrahend));
+		return bdDifference;
+	}
+
+	/**
+	 * Returns of two Item Codes totals
+	 * */
+	public static BigDecimal getReportItemSum(Timestamp fromDate,
+			Timestamp thruDate, String reportId,
+			String reportItemCodeFirstAddend, String reportItemCodeSecondAddend) {
+
+		BigDecimal bdSum = getReportItemTotals(fromDate, thruDate, reportId,
+				reportItemCodeFirstAddend).add(
+				getReportItemTotals(fromDate, thruDate, reportId,
+						reportItemCodeSecondAddend));
+
+		return bdSum;
+	}
+
+	/***
+	 * Utility for getting Report Item Id given reportId and reportItemCode
+	 * */
+	private static String getReportItemId(String reportId, String reportItemCode) {
+		EntityConditionList<EntityExpr> sasraReportItemConditions = EntityCondition
+				.makeCondition(UtilMisc.toList(EntityCondition.makeCondition(
+						"reportId", EntityOperator.EQUALS, reportId),
+						EntityCondition.makeCondition("code",
+								EntityOperator.EQUALS, reportItemCode)),
+						EntityOperator.AND);
+
+		List<GenericValue> sasraReportItemELI = null;
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		try {
+			sasraReportItemELI = delegator.findList("SasraReportItem",
+					sasraReportItemConditions, null, null, null, false);
+
+		} catch (GenericEntityException e2) {
+			e2.printStackTrace();
+		}
+
+		String reportItemId = null;
+		for (GenericValue genericValue : sasraReportItemELI) {
+			reportItemId = genericValue.getString("reportItemId");
+		}
+		return reportItemId;
+	}
+
+	/***
+	 * Utility for getting list of accountIds for a given report Item Id
+	 * */
+	private static List<String> getAccountIds(String reportItemId) {
+		List<String> listAccountIds = new ArrayList<String>();
+
+		// Get the list of account ids that will be summed up for this
+		// reportItem
+		EntityConditionList<EntityExpr> sasraReportGlAccountConditions = EntityCondition
+				.makeCondition(UtilMisc.toList(EntityCondition.makeCondition(
+						"reportItemId", EntityOperator.EQUALS, reportItemId)),
+						EntityOperator.AND);
+
+		List<GenericValue> sasraReportGlAccountELI = null;
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		try {
+			sasraReportGlAccountELI = delegator.findList(
+					"SasraReportGlAccount", sasraReportGlAccountConditions,
+					null, null, null, false);
+
+		} catch (GenericEntityException e2) {
+			e2.printStackTrace();
+		}
+
+		// String reportItemId = null;
+		for (GenericValue genericValue : sasraReportGlAccountELI) {
+			// reportItemId = genericValue.getString("reportItemId");
+			// Add to list
+			listAccountIds.add(genericValue.getString("glAccountId"));
+		}
+
+		return listAccountIds;
+	}
+
+	/***
+	 * Computes the Total for the list of accounts provided
+	 * */
+	private static BigDecimal getAccountTotal(Timestamp fromDate,
+			Timestamp thruDate, List<String> listAccountIds) {
+		// TODO Auto-generated method stub
+		BigDecimal bdTotal = BigDecimal.ZERO;
+		for (String accountId : listAccountIds) {
+			bdTotal = bdTotal
+					.add(getAccountTotal(fromDate, thruDate, accountId));
+		}
+		return bdTotal;
+	}
+
+	/***
+	 * Get total for a give account
+	 * */
+	private static BigDecimal getAccountTotal(Timestamp fromDate,
+			Timestamp thruDate, String accountId) {
+		// TODO Auto-generated method stub
+
+		BigDecimal bdBalanceAmount = BigDecimal.ZERO;
+
+		BigDecimal totalDebitsToOpeningDates = getDebitCreditTotalBalanceByDate(
+				fromDate, "D", accountId);
+		BigDecimal totalDebitsToEndingDates = getDebitCreditTotalBalanceByDate(
+				thruDate, "D", accountId);
+
+		BigDecimal totalCreditsToOpeningDates = getDebitCreditTotalBalanceByDate(
+				fromDate, "C", accountId);
+		BigDecimal totalCreditsToEndingDates = getDebitCreditTotalBalanceByDate(
+				thruDate, "C", accountId);
+
+		GenericValue glAccount = getGLAccount(accountId);
+
+		Boolean isDebit = null;
+		try {
+			isDebit = UtilAccounting.isDebitAccount(glAccount);
+		} catch (GenericEntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (isDebit == null) {
+			return BigDecimal.ZERO;
+		}
+
+		if (isDebit) {
+			// return Debit Balance
+			bdBalanceAmount = totalDebitsToEndingDates
+					.subtract(totalCreditsToEndingDates);
+
+		} else {
+			// return Credit Balance
+			bdBalanceAmount = totalCreditsToEndingDates
+					.subtract(totalDebitsToEndingDates);
+		}
+
+		return bdBalanceAmount;
+	}
+
+	private static GenericValue getGLAccount(String accountId) {
+
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		GenericValue glAccount = null;
+		try {
+			glAccount = delegator.findOne("GlAccount",
+					UtilMisc.toMap("glAccountId", accountId), false);
+		} catch (GenericEntityException e2) {
+			e2.printStackTrace();
+		}
+		return glAccount;
+	}
+
+	private static BigDecimal getDebitCreditTotalBalanceByDate(
+			Timestamp transactionDate, String debitCredit, String accountId) {
+		EntityConditionList<EntityExpr> acctgTransEntrySumsConditions = EntityCondition
+				.makeCondition(UtilMisc.toList(EntityCondition
+						.makeCondition("organizationPartyId",
+								EntityOperator.EQUALS, "Company"),
+						EntityCondition.makeCondition("glAccountId",
+								EntityOperator.EQUALS, accountId)
+
+						, EntityCondition.makeCondition("isPosted",
+								EntityOperator.EQUALS, "Y")
+
+						, EntityCondition.makeCondition("debitCreditFlag",
+								EntityOperator.EQUALS, debitCredit)
+
+						, EntityCondition.makeCondition("glFiscalTypeId",
+								EntityOperator.EQUALS, "ACTUAL")
+
+						, EntityCondition.makeCondition("transactionDate",
+								EntityOperator.LESS_THAN, transactionDate)),
+						EntityOperator.AND);
+
+		List<GenericValue> acctgTransEntrySumsELI = null;
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		try {
+			acctgTransEntrySumsELI = delegator.findList("AcctgTransEntrySums",
+					acctgTransEntrySumsConditions, null, null, null, false);
+
+		} catch (GenericEntityException e2) {
+			e2.printStackTrace();
+		}
+
+		// String reportItemId = null;
+		BigDecimal bdAmount = BigDecimal.ZERO;
+		for (GenericValue genericValue : acctgTransEntrySumsELI) {
+			// reportItemId = genericValue.getString("reportItemId");
+
+			bdAmount = bdAmount.add(genericValue.getBigDecimal("amount"));
+		}
+		return bdAmount;
 	}
 
 }
