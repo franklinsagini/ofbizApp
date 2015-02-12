590c1e16106c140b5c1dcb045b28e6665cf8934b
Japheth Odonya <jodonya@gmail.com>
2015-02-12 11:26:30 +0300

    Sasra Report Service

---
+++ applications/accounting/src/org/ofbiz/accounting/ledger/SasraReportsService.java
@@ -0,0 +1,42 @@
+package org.ofbiz.accounting.ledger;
+
+import java.math.BigDecimal;
+
+public class SasraReportsService {
+	
+	public static BigDecimal getReportItemTotals(String reportId, String reportItemCode){
+		
+		// TODO Fix the code
+		return BigDecimal.ZERO;
+	}
+	
+	
+	public static BigDecimal getReportItemRatio(String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
+		
+		// TODO Fix the code
+		return BigDecimal.ZERO;
+	}
+	
+
+	public static BigDecimal getReportItemRatioPercentage(String reportId, String reportItemCodeNumerator, String reportItemCodeDenominator){
+		
+		// TODO Fix the code return as a percentage * 100
+		return BigDecimal.ZERO;
+	}
+	
+	public static BigDecimal getReportItemDifference(String reportId, String reportItemCodeMinuend, String reportItemCodeSubtrahend){
+		
+		// TODO Fix the code
+		
+		return BigDecimal.ZERO;
+	}
+	
+
+	public static BigDecimal getReportItemSum(String reportId, String reportItemCodeFirstAddend, String reportItemCodeSecondAddend){
+		
+		// TODO Fix the code
+		
+		return BigDecimal.ZERO;
+	}
+
+}
