2837bde0c50dfe0c2637dde4f498c188a8f1202a
Japheth Odonya <jodonya@gmail.com>
2015-02-21 02:06:28 +0300

    Expectation Reports

---
+++ applications/remittancereports/webapp/remittancereports/WEB-INF/actions/reports/stationExpectedReceivedReport.groovy
@@ -0,0 +1,83 @@
+import org.ofbiz.base.util.UtilMisc;
+
+import org.ofbiz.entity.Delegator;
+
+month = parameters.month
+//stationId = parameters.stationId
+
+//Get Station Number
+//station = delegator.findOne("Station", [stationId : stationId], false);
+
+//Station Number
+//stationNumber = station.stationNumber
+
+
+class ExpectReceive{
+	def stationNumber
+	def stationName
+	def month
+	def expected
+	def received
+	def variance
+}
+
+def varianceList = [];
+def expectedReceived
+
+def member
+//Get Expectation Sent for this stationNumber
+expectationList = delegator.findByAnd("ExpectationStationTotalSentSummary",  [month: month], null, false);
+
+
+//Buid a variance list
+
+//context.stationName = station.name;
+context.month = month
+
+expectationList.eachWithIndex { expectItem, index ->
+	
+//	def stationNumber
+//	def stationName
+//	def month
+//	def expected
+//	def received
+//	def variance
+	
+	expectedReceived = new ExpectReceive();
+	expectedReceived.stationNumber = expectItem.stationNumber
+	
+	
+	expectedReceived.month = month
+	expectedReceived.stationNumber = expectItem.stationNumber
+	
+	//Get Member
+	stationNumber = expectedReceived.stationNumber;
+	stationList = delegator.findByAnd("Station",  [stationNumber : stationNumber], null, false);
+	stationName = "";
+	stationList.eachWithIndex { stationItem, stationIndex ->
+		stationName = stationItem.name;
+	}
+	
+	expectedReceived.stationName = stationName;
+	
+	
+	expectedReceived.expected = expectItem.amount;
+	
+	receivedTotal = new BigDecimal(0.0);
+	receivedList = delegator.findByAnd("ExpectationStationTotalReceivedSummary",  [stationNumber : expectedReceived.stationNumber, month: month], null, false);
+	receivedList.eachWithIndex { receivedItem, receivedItemIndex ->
+		receivedTotal = receivedTotal + receivedItem.amount;
+	}
+	
+	expectedReceived.received = receivedTotal
+	
+	expectedReceived.variance = expectedReceived.expected - expectedReceived.received;
+	
+	
+	
+	
+	
+	varianceList << expectedReceived
+}
+
+context.varianceList = varianceList;
