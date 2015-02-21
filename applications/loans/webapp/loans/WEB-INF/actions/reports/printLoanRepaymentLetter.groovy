a8133437761f08745554677544cd0be6d1be132e
Japheth Odonya <jodonya@gmail.com>
2015-02-22 01:49:06 +0300

    Added Reports to Loans

---
+++ applications/loans/webapp/loans/WEB-INF/actions/reports/printLoanRepaymentLetter.groovy
@@ -0,0 +1,41 @@
+import groovy.time.*
+
+loanApplicationId = parameters.loanApplicationId
+lloanApplicationId = loanApplicationId.toLong();
+context.title = "Chai Sacco"
+
+if (loanApplicationId) {
+	loanApplication = delegator.findOne("LoanApplication", [loanApplicationId : lloanApplicationId], false);
+   context.loanApplication = loanApplication;
+}
+
+//Get the LoanProduct
+
+loanProduct = delegator.findOne("LoanProduct", [loanProductId : loanApplication.loanProductId], false);
+context.loanProduct = loanProduct;
+
+member = delegator.findOne("Member", [partyId : loanApplication.partyId], false);
+context.member = member;
+
+salutation = delegator.findOne("Salutation", [salutationId : member.salutationId], false);
+context.salutation = salutation;
+
+startDate = loanApplication.repaymentStartDate;
+duration = loanApplication.repaymentPeriod;
+use(TimeCategory) {
+	lastDate = startDate  + duration.toInteger().month;
+}
+
+context.startDate = startDate;
+context.lastDate = lastDate;
+
+currentContributionAmt = 0
+newMemberDepostContributionAmt = 0
+loanDeductionAmt = 0
+context.currentContributionAmt = currentContributionAmt;
+context.minimumShareContribution = newMemberDepostContributionAmt;
+context.loanRepayment = loanDeductionAmt;
+totalDeduction = newMemberDepostContributionAmt + loanDeductionAmt;
+context.totalDeduction = totalDeduction;
+
+return
