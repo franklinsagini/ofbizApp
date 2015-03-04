5156807f87bdeb087dec71eb493288259a20c18c
Samoei Phil <phil@samphiltech.com>
2015-03-04 19:03:06 +0300

    Fixing Gl Account Display: Introducing Running Balance

--- applications/accounting/webapp/accounting/WEB-INF/actions/gl/gl.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/gl/gl.groovy
@@ -21,8 +21,16 @@ runningBalance = BigDecimal.ZERO
 totalDebits = BigDecimal.ZERO
 totalCredits = BigDecimal.ZERO
 finalentriesList = [];
+count = 0;
+balanceType = "";
 entriesList.each { entry ->
-    if(entry.debitCreditFlag == "D") {
+  if (count == 0) {
+    balanceType = entry.debitCreditFlag;
+  }
+
+
+  if (balanceType == "D") {
+        if(entry.debitCreditFlag == "D") {
       if (entry.amount) {
       runningBalance = runningBalance + entry.amount
       totalDebits = totalDebits + entry.amount
@@ -33,7 +41,25 @@ entriesList.each { entry ->
        totalCredits = totalCredits + entry.amount
       }
     }
+  }else{
+        if(entry.debitCreditFlag == "C") {
+      if (entry.amount) {
+      runningBalance = runningBalance + entry.amount
+      totalDebits = totalDebits + entry.amount
+      }
+    } else {
+      if (entry.amount) {
+        runningBalance = runningBalance - entry.amount
+       totalCredits = totalCredits + entry.amount
+      }
+    }
+  }
+
+
+
+
     finalentriesList.add("transactionDate" : entry.transactionDate, "acctgTransId" : entry.acctgTransId, "accountName" : entry.accountName,"acctgTransTypeId" : entry.acctgTransTypeId, "debitCreditFlag" : entry.debitCreditFlag, "amount" : entry.amount, "runningBalance" : runningBalance)
+    count = count + 1;
 }
 
 
