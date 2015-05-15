61947d74b79393979b37842c13d339c4f7729e81
Samoei Phil <phil@samphiltech.com>
2015-05-15 12:50:01 +0300

    WORKING ON INTER BRANCH REPORTS

---
+++ applications/accounting/webapp/accounting/WEB-INF/actions/interbranch/BranchTransactionsReport.groovy
@@ -0,0 +1,8 @@
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.base.util.UtilMisc;
+
+
+branchId = parameters.get("organizationPartyId");
+
+System.out.println("####################################### BranchId: " + branchId)
