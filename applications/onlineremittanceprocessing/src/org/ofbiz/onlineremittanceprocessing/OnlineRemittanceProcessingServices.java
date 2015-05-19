f7cc8f5b554e25f9807beb8eb7af6a1a9487ac3d
Japheth Odonya <jodonya@gmail.com>
2015-05-19 22:01:14 +0300

    Managing the Online Stations in place - add all, remove all, add one, remove one

--- applications/onlineremittanceprocessing/src/org/ofbiz/onlineremittanceprocessing/OnlineRemittanceProcessingServices.java
+++ applications/onlineremittanceprocessing/src/org/ofbiz/onlineremittanceprocessing/OnlineRemittanceProcessingServices.java
@@ -1,17 +1,150 @@
 package org.ofbiz.onlineremittanceprocessing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import javolution.util.FastMap;
+
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.DelegatorFactoryImpl;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.party.party.SaccoUtility;
 
 public class OnlineRemittanceProcessingServices {
-	
-	public static String addToClearList(HttpServletRequest request,
-			HttpServletResponse response) {
-		//Map<String, Object> result = FastMap.newInstance();
-		//Delegator delegator = (Delegator) request.getAttribute("delegator");
 
+	/****
+	 * Adds all stations online - to the online list
+	 * */
+	public static String addAllStationsOnline(HttpServletRequest request,
+			HttpServletResponse response) {
+		// Map<String, Object> result = FastMap.newInstance();
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+
+		HttpSession session = request.getSession();
+		GenericValue userLogin = (GenericValue) session
+				.getAttribute("userLogin");
+		String userLoginId = userLogin.getString("userLoginId");
+
+		// Get all stations
+		List<GenericValue> stationELI = new ArrayList<GenericValue>();
+		try {
+			stationELI = delegator.findList("Station", null, null, null, null,
+					false);
+		} catch (GenericEntityException e) {
+			e.printStackTrace();
+		}
+
+		List<GenericValue> onlineStationItemELI = new ArrayList<GenericValue>();
+		// Add each station to the online list
+		for (GenericValue genericValue : stationELI) {
+			// Add station to Online Stations List
+			onlineStationItemELI.add(addStationToOnline(genericValue,
+					delegator, userLoginId));
+		}
+
+		try {
+			delegator.storeAll(onlineStationItemELI);
+		} catch (GenericEntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return "success";
+	}
+
+	private static GenericValue addStationToOnline(GenericValue genericValue,
+			Delegator delegator, String userLoginId) {
+
+		GenericValue onlineStationItem;
+		Long onlineStationItemId;
+		onlineStationItemId = SaccoUtility.getNextSequenc("OnlineStationItem");
+		onlineStationItem = delegator.makeValidValue("OnlineStationItem",
+				UtilMisc.toMap("onlineStationItemId", onlineStationItemId,
+						"stationId", genericValue.getString("stationId"),
+						"isActive", "Y", "createdBy", userLoginId));
+		return onlineStationItem;
+	}
+
+	/***
+	 * Clears the online list - removing all stations
+	 * */
+	public static String removeAllStationsOnline(HttpServletRequest request,
+			HttpServletResponse response) {
+		// Map<String, Object> result = FastMap.newInstance();
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+
+		// Get all stations
+		List<GenericValue> olineStationItemELI = new ArrayList<GenericValue>();
+		try {
+			olineStationItemELI = delegator.findList("OnlineStationItem", null,
+					null, null, null, false);
+		} catch (GenericEntityException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			delegator.removeAll(olineStationItemELI);
+		} catch (GenericEntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "success";
+	}
+
+	/***
+	 * Removes a station from online
+	 * */
+	public static String removeStationOnline(HttpServletRequest request,
+			HttpServletResponse response) {
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+		
+		String onlineStationItemIdStr = (String) request.getParameter("onlineStationItemId");
+		
+		onlineStationItemIdStr = onlineStationItemIdStr.replaceAll(",", "");
+		Long onlineStationItemId = Long.valueOf(onlineStationItemIdStr);
+		
+		
+		try {
+			delegator.removeByCondition("OnlineStationItem", EntityCondition.makeCondition("onlineStationItemId",
+										onlineStationItemId));
+		} catch (GenericEntityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+		return "success";
+	}
+
+	/***
+	 * Returns the total number of Stations that are online
+	 * 
+	 * */
+	public static Long getOnlineStationsCount() {
+		Long stationCount = 0L;
+		Delegator delegator = DelegatorFactoryImpl.getDelegator(null);
+
+		List<GenericValue> olineStationItemELI = new ArrayList<GenericValue>();
+		try {
+			olineStationItemELI = delegator.findList("OnlineStationItem", null,
+					null, null, null, false);
+		} catch (GenericEntityException e) {
+			e.printStackTrace();
+		}
+
+		stationCount = (long) olineStationItemELI.size();
+		
+		return stationCount;
 	}
 
 }
