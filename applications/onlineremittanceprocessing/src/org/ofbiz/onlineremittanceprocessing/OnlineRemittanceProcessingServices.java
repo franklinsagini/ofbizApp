2ca686fbd046d5b2b4f8d30fcf3c99cacee1fc1b
Japheth Odonya <jodonya@gmail.com>
2015-05-19 19:50:48 +0300

    Adding stations to online processing

---
+++ applications/onlineremittanceprocessing/src/org/ofbiz/onlineremittanceprocessing/OnlineRemittanceProcessingServices.java
@@ -0,0 +1,17 @@
+package org.ofbiz.onlineremittanceprocessing;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OnlineRemittanceProcessingServices {
+	
+	public static String addToClearList(HttpServletRequest request,
+			HttpServletResponse response) {
+		//Map<String, Object> result = FastMap.newInstance();
+		//Delegator delegator = (Delegator) request.getAttribute("delegator");
+
+
+		return "success";
+	}
+
+}
