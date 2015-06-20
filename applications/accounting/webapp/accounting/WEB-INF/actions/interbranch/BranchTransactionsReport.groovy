0d6b981fcee847099249c6891faa73828862e9a5
Samoei Phil <phil@samphiltech.com>
2015-06-20 21:24:23 +0300

    WORKING ON INTERBRANCH REPORTS

--- applications/accounting/webapp/accounting/WEB-INF/actions/interbranch/BranchTransactionsReport.groovy
+++ applications/accounting/webapp/accounting/WEB-INF/actions/interbranch/BranchTransactionsReport.groovy
@@ -4,31 +4,9 @@ import org.ofbiz.base.util.UtilMisc;
 import javolution.util.FastList;
 
 
-branchId = parameters.get("organizationPartyId");
-fundingTypeId = parameters.get("fundingTypeId");
-entriesList = [];
+ownedBranchId = parameters.get("partyId");
+owingBranchId = parameters.get("organizationPartyId");
 
-List mainAndExprs = FastList.newInstance();
-mainAndExprs.add(EntityCondition.makeCondition("glAccountId", EntityOperator.EQUALS, parameters.glAccountId));
-
-if (fundingTypeId == "1") {
-  //USE HQ ATM Settlement Account  gl_account_id = '206'
-
-  //organization_party_id == branchId
-  //party_id == memberId
-
-  mainAndExprs.add(EntityCondition.makeCondition("glAccountId", EntityOperator.EQUALS, "206"));
-  mainAndExprs.add(EntityCondition.makeCondition("organizationPartyId", EntityOperator.EQUALS, branchId));
-
-entriesList = delegator.findList("AcctgTransEntry", EntityCondition.makeCondition(mainAndExprs, EntityOperator.AND), UtilMisc.toSet("transactionDate", "acctgTransId", "accountName","acctgTransTypeId", "debitCreditFlag", "amount"), UtilMisc.toList("acctgTransEntrySeqId"), null, false);
-
-}
-if (fundingTypeId == "2") {
-  System.out.println("####################################### fundingTypeId: " + fundingTypeId)
-}
-if (fundingTypeId == "3") {
-  System.out.println("####################################### fundingTypeId: " + fundingTypeId)
-}
-
-System.out.println("####################################### BranchId: " + branchId)
+context.ownedBranchId = ownedBranchId
+context.owingBranchId = owingBranchId
 
