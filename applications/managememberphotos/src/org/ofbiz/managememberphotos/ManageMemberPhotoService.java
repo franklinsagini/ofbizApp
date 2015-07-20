56c962da7b17e2766d9e485b1dca95ca8d7a0b14
Japheth Odonya <jodonya@gmail.com>
2015-07-20 23:37:55 +0300

    Adding manage member photos

---
+++ applications/managememberphotos/src/org/ofbiz/managememberphotos/ManageMemberPhotoService.java
@@ -0,0 +1,65 @@
+package org.ofbiz.managememberphotos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+/***
+ * @author Japheth Odonya  @when Jul 20, 2015 5:42:36 PM
+ * Loading Member Photos
+ * 
+ * org.ofbiz.managememberphotos.ManageMemberPhotoService.loadMembersPhotos(sourcepath, destinationPath, userLogin)
+ * */
+public class ManageMemberPhotoService {
+	
+	public static String loadMembersPhotos(String sourcepath, String destinationPath, Map<String, String> userLogin){
+		String success = "success";
+		
+		File folder = new File("/F:/projects/vergeofbiz/vergesacco/sourceimages");
+		String imagesPath = "";
+		String destinationpath = "";
+		if (!folder.exists()){
+			imagesPath = "/home/online/sourceimages";
+			destinationpath = "/home/samuel/installations/chaisacco/framework/images/members"; 
+		} else{
+			imagesPath = "/F:/projects/vergeofbiz/vergesacco/sourceimages";
+			destinationpath = "/F:/projects/vergeofbiz/vergesacco/framework/images/members"; 
+		}
+
+		//FileUtils.copyDirectory(srcDir, destDir);
+		
+//		File source = new File("H:\\work-temp\\file");
+//		File dest = new File("H:\\work-temp\\file2");
+//		try {
+//		    FileUtils.copyDirectory(source, dest);
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
+		
+		Long partyId = 1000L;
+		String memberId = "";
+		File source = new File(imagesPath+"/"+"photo_"+memberId+".jpg");
+		File dest = new File(destinationpath+"/"+partyId.toString()+"/"+"photourl.jpg");
+		try {
+		    FileUtils.copyDirectory(source, dest);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		//Get 100 members from members without photos and load the photos
+		
+		
+		//load the photo
+		//check in the path directory for photo, id front, id back and signature
+		
+		//create a directory in the destination folder with name as member party id
+		//copy the id into directory named photorurl, idfronturl, idbackurl, signatureurl
+		
+		//repeat until the list of 100 members is zero
+		
+		return success;
+	}
+
+}
